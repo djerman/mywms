b8a4fa98ac619c89d796ecde4703093645743d00
goran <goran.djermanovic@neomatic.io>
2025-07-27 14:58:17 +0200

    Folder rich.client changed

--- rich.client/los.clientsuite/LOS Common/src/de/linogistix/common/util/GraphicUtil.java
+++ rich.client/los.clientsuite/LOS Common/src/de/linogistix/common/util/GraphicUtil.java
@@ -10,7 +10,7 @@ package de.linogistix.common.util;
 //import com.sun.image.codec.jpeg.JPEGCodec;
 //import com.sun.image.codec.jpeg.JPEGEncodeParam;
 //import com.sun.image.codec.jpeg.JPEGImageEncoder;
-//import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
+import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
 import de.linogistix.common.gui.object.IconType;
 import java.awt.AWTException;
 import java.awt.AlphaComposite;
@@ -37,10 +37,6 @@ import javax.swing.ImageIcon;
 //import static org.hibernate.cache.Timestamper.next;
 import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
-//zamena za image writer
-import javax.imageio.ImageWriter;
-import javax.imageio.ImageIO;
-import java.util.Iterator;
 
 
 /**
@@ -214,11 +210,7 @@ public class GraphicUtil extends Component {
 
             //dgrys new code
             //TODO dgrys test it
-            //JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpeg").next();
-            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpeg");
-            if (writers.hasNext()) {
-            ImageWriter imageWriter = writers.next();
-            // користи writer као обичан ImageWriter
+            JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpeg").next();
             ImageOutputStream ios = ImageIO.createImageOutputStream(fos);
             imageWriter.setOutput(ios);
             
@@ -227,9 +219,6 @@ public class GraphicUtil extends Component {
             imageWriter.write(imageMetaData, new IIOImage(image, null, null), null);
             ios.close();
             imageWriter.dispose();
-            }
-            
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
